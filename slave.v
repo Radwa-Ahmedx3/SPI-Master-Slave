26b7af6f93bc1fdcdbb8c713bf7aad0eb2b5f3c3
eyadnoah99 <eyadnoah99@gmail.com>
2026-08-05 12:51:38 +0300

    Add files via upload

--- slave.v
+++ slave.v
@@ -22,7 +22,6 @@ reg first_bit;
 always @(posedge clk or negedge rstn) begin
 	if(~rstn) begin
 		cs <= idle;
-		read_addr_flag <= 0;
 	end
 
 	else begin
@@ -72,68 +71,64 @@ always @(posedge clk or negedge rstn) begin
 		rx_data <= 0;
 		counter <= 0;
 		counter_4_read_data <= 0;
-		internal_register <= 0;
 	end
 
 	else begin
 		case(cs)
 			check:begin
-				internal_register [9] <= mosi;
+				rx_data [9] <= mosi;
 				counter  <= counter + 1;
 			end
 			write: begin
                 if (counter < 10) begin
-                   internal_register[9 - counter] <= mosi;
+                   rx_data[9 - counter] <= mosi;
                     counter  <= counter + 1;
                     rx_valid <= 0;
                 end 
                 else begin 
-                    counter  <= 0;
-                    rx_data  <= internal_register; 
+                    counter  <= 0; 
                     rx_valid <= 1;
                 end
             end
 			read_addr: begin
                 if (counter < 10) begin
-                    internal_register[9 - counter] <= mosi;
+                    rx_data[9 - counter] <= mosi;
                     counter  <= counter + 1;
                     rx_valid <= 0;
                 end 
                 else begin 
                     counter <= 0;
-                    rx_data  <= internal_register; 
-                    rx_valid <= 1;
-                    read_addr_flag <= 1; 
+                    rx_valid <= 1; 
                 end
+                read_addr_flag <= 1;
             end
 			read_data: begin
 				if (counter < 10) begin
-					internal_register[9 - counter] <= mosi;
+					rx_data[9 - counter] <= mosi;
 					counter <= counter + 1;
 					rx_valid <= 0;
 				end
 				else begin 
 					counter <= 0;
-					rx_data <= internal_register;
 					rx_valid <= 1;
 				end
-				if (tx_valid) begin
-					internal_register   <= tx_data;
-					miso <= tx_data[7]; 
-					counter_4_read_data <= 1;
+
+				if (tx_valid) begin        
+					miso <= tx_data[7];                   
+					counter_4_read_data <= 1;             
 				end 
-				else if (counter_4_read_data > 0 && counter_4_read_data <= 7) begin
-					miso <= internal_register[7 - counter_4_read_data];
+				else if (counter_4_read_data >= 1 && counter_4_read_data < 8) begin
+					miso <= tx_data [7 - counter_4_read_data]; 
 					counter_4_read_data <= counter_4_read_data + 1;
 				end 
 				else if (counter_4_read_data == 8) begin
-					counter_4_read_data <= 0;
+					counter_4_read_data <= 0;             
 				end
-				read_addr_flag <= 0;
+
+				read_addr_flag <= 0; 
 			end
 			default:begin
 				counter <= 0;
-				read_addr_flag <=0;
 				counter_4_read_data <= 0;
 			end
 		endcase
